092fa13b34e051bc988b7cb40c05d5bbe33801dd
Oliver Ballon <oliverballon1@gmail.com>
2020-07-30 12:47:25 +0100

    Gulp minify on save added

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
 const gulp = require('gulp');
 const minify = require('gulp-minify');
 const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename')
-
-
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass')
+const minifystyle = require('gulp');
 
 
 gulp.task('minify-js', function() {
-  gulp.src(['js/app.js'])
+  return gulp.src(['js/app.js'])
     .pipe(minify())
     .pipe(gulp.dest('js/'))
 });
 
+gulp.task('sass', function () {
+	return gulp.src('styles/main.scss')
+		.pipe(sass())
+		.pipe(rename('main.css'))
+		.pipe(gulp.dest('./styles/'));
+}); 
+
 gulp.task('minify-css', () => {
-    return gulp.src('styles/*.css')
+    return gulp.src('styles/main.css')
       .pipe(cleanCSS({compatibility: 'ie8'})
       .pipe(rename({suffix: '.min'})))
       .pipe(gulp.dest('./styles/'));
   });
+
+  gulp.task( 'watch', function () {
+    //                   * all files to watch
+      return gulp.watch('./styles/*.scss',
+    //                      task to run
+              gulp.task( 'minifystyle' ) );
+    } );
+    //^ this will automatically run 'minify-sass'
+
+ 
+
+
+  gulp.task( 'minifystyle', gulp.series( 'sass', 'minify-css' ) );
+  
+  
